71c75b99c25ec30fcbf94e2ac7b668983692d655
deepu <deepukasyap143@gamil.com>
2026-06-02 15:41:07 +0530

    02/06/2026

--- dotNotCoreWithAngular/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ dotNotCoreWithAngular/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8eecd5df232adc382a3fceb51aece68a9044ca2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6342b6b88d31a603a72a3cd735a44086a37fe17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
